3ed03196d2ffd9b33e3a37dc42d64467dffe73cd
Tuana Çetinkaya <b21985164@cs.hacettepe.edu.tr>
2022-04-12 18:20:24 +0300

    login no css

--- placeholder-frontend/src/components/welcome-screen/Login.js
+++ placeholder-frontend/src/components/welcome-screen/Login.js
@@ -1,75 +1,73 @@
 import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import {makeStyles} from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import {useHistory} from "react-router-dom";
-import {AuthService} from "../service/AuthService";
-import Grid from "@material-ui/core/Grid";
-import Link from "@material-ui/core/Link";
-import Box from "@material-ui/core/Box";
-import {Snackbar} from "@material-ui/core";
-import Copyright from "../component/common/Copyright";
-import {Alert} from "@material-ui/lab";
-import {delay} from "../util/async";
-import {MemberService} from "../service/MemberService";
 
-const useStyles = makeStyles((theme) => ({
-    paper: {
-        marginTop: theme.spacing(8),
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-    },
-    avatar: {
-        margin: theme.spacing(1),
-        backgroundColor: theme.palette.secondary.main,
-    },
-    form: {
-        width: '100%', // Fix IE 11 issue.
-        marginTop: theme.spacing(1),
-    },
-    submit: {
-        margin: theme.spacing(3, 0, 2),
-    },
-}));
+
+// const useStyles = makeStyles((theme) => ({
+//     paper: {
+//         marginTop: theme.spacing(8),
+//         display: 'flex',
+//         flexDirection: 'column',
+//         alignItems: 'center',
+//     },
+//     avatar: {
+//         margin: theme.spacing(1),
+//         backgroundColor: theme.palette.secondary.main,
+//     },
+//     form: {
+//         width: '100%', // Fix IE 11 issue.
+//         marginTop: theme.spacing(1),
+//     },
+//     submit: {
+//         margin: theme.spacing(3, 0, 2),
+//     },
+// }));
 
 export default function Login() {
-    const classes = useStyles();
-    const history = useHistory()
+    // const classes = useStyles();
+    // const history = useHistory()
 
     const [password, setPassword] = React.useState("");
     const [username, setUsername] = React.useState("");
 
-    const [open, setSnackbarOpen] = React.useState(false);
-    const [severity, setSnackbarSeverity] = React.useState("success");
-    const [snackbarMessage, setSnackbarMessage] = React.useState("Welcome back!");
 
+    const handleLogin = (event) => {
+        event.preventDefault();
+        // console.log("Sign in button clicked.");
+        // AuthService.login(username, password).then(r => {
+        //     console.log("Response: " + JSON.stringify(r.data));
+        //     AuthService.saveJwtToken(r.data.token);
+        //     AuthService.saveAuthRoles(r.data.auth);
+        //     setSnackbarSeverity("success");
+        //     setSnackbarMessage("Welcome back!");
+        //     setSnackbarOpen(true);
 
-    const handleSnackbarClose = (event, reason) => {
-        if (reason === 'clickaway') {
-            return;
-        }
+        //     delay(1000).then(() => {
+        //         if(r.data.enrollmentCount === 0){
+        //             history.push("/subclub-recommendation");
+        //         }
+        //         else{
+        //             history.push("/")
+        //         }
+        //     })
+        // }).catch(e => {
+        //     setSnackbarSeverity("error");
+        //     if (e.response !== undefined && e.response.status === 401) {
+        //         setSnackbarMessage("Entered credentials are incorrect.");
+        //     } else {
+        //         setSnackbarMessage("Something went wrong!");
+        //     }
+        //     setSnackbarOpen(true);
+        // })
+    }
 
-        setSnackbarOpen(false);
-    };
 
     return (
-        <Container component="main" maxWidth="xs">
-            <CssBaseline/>
-            <div className={classes.paper}>
+        <div component="main" maxWidth="xs">
+            <div>
 
-                <Avatar className={classes.avatar}> <LockOutlinedIcon/> </Avatar>
-                <Typography component="h1" variant="h5"> Login </Typography>
+                <h3> Login </h3>
 
-                <form className={classes.form} noValidate>
-                    <TextField
+                <form className='login-form' noValidate>
+                    <input type='text'
                         variant="outlined"
                         margin="normal"
                         required
@@ -83,80 +81,42 @@ export default function Login() {
                             setUsername(event.target.value)
                         }}
                     />
-                    <TextField
+                    <input type='password'
                         variant="outlined"
                         margin="normal"
                         required
                         fullWidth
                         name="password"
                         label="Password"
-                        type="password"
                         id="password"
                         autoComplete="current-password"
                         onChange={(event) => {
                             setPassword(event.target.value)
                         }}
                     />
-                    <FormControlLabel
+                    {/* <FormControlLabel
                         control={<Checkbox value="remember" color="primary"/>}
                         label="Remember me"
-                    />
-                    <Button
-                        type="submit"
+                    /> */}
+                    <button type='submit'
                         fullWidth
                         variant="contained"
                         color="primary"
-                        className={classes.submit}
-                        onClick={(event) => {
-                            event.preventDefault();
-                            console.log("Sign in button clicked.");
-                            AuthService.login(username, password).then(r => {
-                                console.log("Response: " + JSON.stringify(r.data));
-                                AuthService.saveJwtToken(r.data.token);
-                                AuthService.saveAuthRoles(r.data.auth);
-                                setSnackbarSeverity("success");
-                                setSnackbarMessage("Welcome back!");
-                                setSnackbarOpen(true);
-
-                                delay(1000).then(() => {
-                                    if(r.data.enrollmentCount === 0){
-                                        history.push("/subclub-recommendation");
-                                    }
-                                    else{
-                                        history.push("/")
-                                    }
-                                })
-                            }).catch(e => {
-                                setSnackbarSeverity("error");
-                                if (e.response !== undefined && e.response.status === 401) {
-                                    setSnackbarMessage("Entered credentials are incorrect.");
-                                } else {
-                                    setSnackbarMessage("Something went wrong!");
-                                }
-                                setSnackbarOpen(true);
-                            })
-                        }}
+                        className='submit'
+                        onClick={handleLogin}
                     >
                         Sign In
-                    </Button>
-                    <Grid container>
+                    </button>
+                    {/* <Grid container>
                         <Grid item xs>
                             <Link href="/password-reset" variant="body2"> Forgot password? </Link>
                         </Grid>
                         <Grid item>
                             <Link href="/sign-up" variant="body2"> {"Don't have an account? Sign up."} </Link>
                         </Grid>
-                    </Grid>
+                    </Grid> */}
                 </form>
             </div>
-            <Box mt={8}>
-                <Copyright/>
-            </Box>
-            <Snackbar open={open} autoHideDuration={6000} onClose={handleSnackbarClose}>
-                <Alert onClose={handleSnackbarClose} severity={severity}>
-                    {snackbarMessage}
-                </Alert>
-            </Snackbar>
-        </Container>
+        </div>
     );
 }
