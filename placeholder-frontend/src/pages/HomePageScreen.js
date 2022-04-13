96acf35bda777ab519518196b1045eb4bb3169d1
mavibirdesmi <desmin>
2022-04-13 22:31:03 +0300

    Added homepagescreen & router

---
+++ placeholder-frontend/src/pages/HomePageScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme();
+
+export default function HomePageScreen() {
+
+  return (
+    <div>
+      <p>Main Page!!</p>
+    </div>
+  )
+}
