72b3319f28600398dced1f65499377787b0ef13e
Tuana Çetinkaya <b21985164@cs.hacettepe.edu.tr>
2022-04-13 04:24:31 +0300

    login component

--- placeholder-frontend/src/components/welcome-screen/Login.js
+++ placeholder-frontend/src/components/welcome-screen/Login.js
@@ -1,122 +1,129 @@
-import React from 'react';
+import * as React from "react";
+import {
+  Avatar,
+  Button,
+  CssBaseline,
+  Card,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+  Link,
+  Grid,
+  Box,
+  Typography,
+  Container,
+} from "@mui/material";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import './Login.css'
 
+function Copyright(props) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link
+        color="inherit"
+        href="https://github.com/BBM384-2021/bbm384-2022-demo-final-placeholder"
+      >
+        Placeholder Team
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
 
-// const useStyles = makeStyles((theme) => ({
-//     paper: {
-//         marginTop: theme.spacing(8),
-//         display: 'flex',
-//         flexDirection: 'column',
-//         alignItems: 'center',
-//     },
-//     avatar: {
-//         margin: theme.spacing(1),
-//         backgroundColor: theme.palette.secondary.main,
-//     },
-//     form: {
-//         width: '100%', // Fix IE 11 issue.
-//         marginTop: theme.spacing(1),
-//     },
-//     submit: {
-//         margin: theme.spacing(3, 0, 2),
-//     },
-// }));
+const theme = createTheme();
 
 export default function Login() {
-    // const classes = useStyles();
-    // const history = useHistory()
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+  };
 
-    const [password, setPassword] = React.useState("");
-    const [username, setUsername] = React.useState("");
-
-
-    const handleLogin = (event) => {
-        event.preventDefault();
-        // console.log("Sign in button clicked.");
-        // AuthService.login(username, password).then(r => {
-        //     console.log("Response: " + JSON.stringify(r.data));
-        //     AuthService.saveJwtToken(r.data.token);
-        //     AuthService.saveAuthRoles(r.data.auth);
-        //     setSnackbarSeverity("success");
-        //     setSnackbarMessage("Welcome back!");
-        //     setSnackbarOpen(true);
-
-        //     delay(1000).then(() => {
-        //         if(r.data.enrollmentCount === 0){
-        //             history.push("/subclub-recommendation");
-        //         }
-        //         else{
-        //             history.push("/")
-        //         }
-        //     })
-        // }).catch(e => {
-        //     setSnackbarSeverity("error");
-        //     if (e.response !== undefined && e.response.status === 401) {
-        //         setSnackbarMessage("Entered credentials are incorrect.");
-        //     } else {
-        //         setSnackbarMessage("Something went wrong!");
-        //     }
-        //     setSnackbarOpen(true);
-        // })
-    }
-
-
-    return (
-        <div component="main" maxWidth="xs">
-            <div>
-
-                <h3> Login </h3>
-
-                <form className='login-form' noValidate>
-                    <input type='text'
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="username"
-                        label="Username"
-                        name="username"
-                        autoComplete="username"
-                        autoFocus
-                        onChange={(event) => {
-                            setUsername(event.target.value)
-                        }}
-                    />
-                    <input type='password'
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        name="password"
-                        label="Password"
-                        id="password"
-                        autoComplete="current-password"
-                        onChange={(event) => {
-                            setPassword(event.target.value)
-                        }}
-                    />
-                    {/* <FormControlLabel
-                        control={<Checkbox value="remember" color="primary"/>}
-                        label="Remember me"
-                    /> */}
-                    <button type='submit'
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                        className='submit'
-                        onClick={handleLogin}
-                    >
-                        Sign In
-                    </button>
-                    {/* <Grid container>
-                        <Grid item xs>
-                            <Link href="/password-reset" variant="body2"> Forgot password? </Link>
-                        </Grid>
-                        <Grid item>
-                            <Link href="/sign-up" variant="body2"> {"Don't have an account? Sign up."} </Link>
-                        </Grid>
-                    </Grid> */}
-                </form>
-            </div>
-        </div>
-    );
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <Card className="card-view">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 8,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Box
+              component="form"
+              onSubmit={handleSubmit}
+              noValidate
+              sx={{ mt: 1 }}
+            >
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign In
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link href="/register" variant="body2">
+                    {"Don't have an account? Register"}
+                  </Link>
+                </Grid>
+              </Grid>
+            </Box>
+          </Box>
+          <Copyright sx={{ mt: 8, mb: 4 }} />
+        </Card>
+      </Container>
+    </ThemeProvider>
+  );
 }
