2ccf7c128960bfb5bd31118d7dcf1c968c0d6bfa
mirzadal <mirzadal4646@gmail.com>
2022-04-14 20:00:29 +0300

    profile update2

--- placeholder-frontend/src/components/profilepage-screen/Profile.js
+++ placeholder-frontend/src/components/profilepage-screen/Profile.js
@@ -8,33 +8,42 @@ import vectorPencil from "./assets/pencil.png";
 import vectorCamera from "./assets/camera.png";
 import vectorGithub from "./assets/github.png";
 import vectorLinkedin from "./assets/linkedin.png";
+import vectorMore from "./assets/menu-dots.png";
+import vectorDocument from "./assets/document-signed.png";
+import vectorSmile from "./assets/smile.png";
 
 
 export default function Profile() {
 
     return (
         <div className="profileContainer">
-            <img src={cover} className="coverImage"/>
+            <img src={cover} className="coverImage" alt=""/>
             <div className="profileDetails">
                 <div className="profileDetailLeft">
                     <div className="profileDetailRow">
                         <img src={profilePic} alt="" className="profileImage"/>
                         <div>
                             <h3>Desmin Alpaslan</h3>
+                            <h4>54 connections</h4>
+                            <button> <img src={vectorGithub} className="githubImage" alt=""/> </button>
+                            <button> <img src={vectorLinkedin} className="githubImage" alt=""/> </button>
                         </div>
                     </div>
                 </div>
                 <div className="profileDetailRight">
-                    <button>
-                        img src={vectorGithub}
-                    </button>
-                    <button>
-                        img src={vectorLinkedin}
-                    </button>
-                    <button>
-                        Edit profile
-                        img src={vectorPencil}
-                    </button>
+                    <button> Edit profile <img src={vectorPencil} className="githubImage" alt=""/> </button>
+                    <a> <img src={vectorMore} className="more" alt=""/> </a>
+                </div>
+            </div>
+
+            <div className="profileInfo">
+
+                <div className="infoColumn">
+
+                </div>
+
+                <div className="feedColumn">
+
                 </div>
             </div>
         </div>
