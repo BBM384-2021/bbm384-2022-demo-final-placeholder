c794438a369a1530af80dd34c6808b4937b08e2a
mavibirdesmi <b21945795@cs.hacettepe.edu.tr>
2022-04-15 00:26:00 +0300

    Finished main page

--- placeholder-frontend/src/components/homepage-screen/ChatSideBar.js
+++ placeholder-frontend/src/components/homepage-screen/ChatSideBar.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import Box from '@mui/material/Box';
 
 export default function ChatSideBar() {
 
   return (
-    <p>ChatSideBar</p>
-  )
+    <Box 
+      sx={{
+        width:'25%',
+        backgroundColor:'white',
+        borderRadius:'17px',
+        display:'flex'
+      }}
+    >
+      <div style={{ marginLeft: '31px'}}>
+        <h3 style={{ color:'#888888'}}>Chat</h3>
+      </div>
+      
+    </Box>
+  );
 }
--- placeholder-frontend/src/pages/HomePageScreen.js
+++ placeholder-frontend/src/pages/HomePageScreen.js
@@ -1,7 +1,5 @@
 import React from 'react'
-
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-import PrimarySearchAppBar from '../components/homepage-screen/PrimarySearchAppBar';
+import TopBar from '../components/homepage-screen/TopBar';
 import EventSideBar from '../components/homepage-screen/EventSideBar';
 import ChatSideBar from '../components/homepage-screen/ChatSideBar';
 import MainFeed from '../components/homepage-screen/MainFeed';
@@ -11,15 +9,14 @@ import Box from '@mui/material/Box';
 export default function HomePageScreen() {
 
   return (
-    <Box>
-          <div>
-            <PrimarySearchAppBar />
-            <div>
-              <EventSideBar />
-              <MainFeed />
-              <ChatSideBar />
-            </div>
-          </div>
+    <Box sx={{display:'flex', marginLeft:'10px', marginRight:'10px', flexDirection:'column'}}>
+      <TopBar />
+      <div style={{display:'flex', height:'80vh',margin:'0px', justifyContent:'space-between', paddingTop:'2%',
+      fontFamily:'Poppins', flex:'auto'}}>
+        <EventSideBar />
+        <MainFeed />
+        <ChatSideBar />
+      </div>
     </Box>
   )
 }
