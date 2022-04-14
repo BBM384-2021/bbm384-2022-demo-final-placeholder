45d23488255945a09490fd608e5f973c774b9080
mavibirdesmi <b21945795@cs.hacettepe.edu.tr>
2022-04-14 18:31:42 +0300

    Added logo

--- placeholder-frontend/src/pages/HomePageScreen.js
+++ placeholder-frontend/src/pages/HomePageScreen.js
@@ -6,18 +6,20 @@ import EventSideBar from '../components/homepage-screen/EventSideBar';
 import ChatSideBar from '../components/homepage-screen/ChatSideBar';
 import MainFeed from '../components/homepage-screen/MainFeed';
 
-const theme = createTheme();
+import Box from '@mui/material/Box';
 
 export default function HomePageScreen() {
 
   return (
-    <div>
-      <PrimarySearchAppBar />
-      <div>
-        <EventSideBar />
-        <MainFeed />
-        <ChatSideBar />
-      </div>
-    </div>
+    <Box sx={{ display:'flex', flexDirection:'column', height:'100vh',bgcolor: '#F5F5F5'}}>
+          <div>
+            <PrimarySearchAppBar />
+            <div>
+              <EventSideBar />
+              <MainFeed />
+              <ChatSideBar />
+            </div>
+          </div>
+    </Box>
   )
 }
