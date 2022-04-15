176660627db3597e35ecb6c512a44834e67217eb
mavibirdesmi <b21945795@cs.hacettepe.edu.tr>
2022-04-15 08:22:25 +0300

    Profile linking url bilmem ne

--- placeholder-frontend/src/pages/ProfilePageScreen.js
+++ placeholder-frontend/src/pages/ProfilePageScreen.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useParams } from "react-router-dom";
 
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import TopBar from '../components/homepage-screen/TopBar';
@@ -10,14 +11,15 @@ import Box from "@mui/material/Box";
 const theme = createTheme();
 
 export default function ProfilePageScreen() {
-
+    const { user_id } = useParams();
+    console.log(user_id)
   return (
       <Box sx={{display:'flex', marginLeft:'10px', marginRight:'10px', flexDirection:'column'}}>
           <TopBar />
           <div style={{display:'flex', height:'80vh',margin:'0px', justifyContent:'space-between', paddingTop:'2%',
               fontFamily:'Poppins', flex:'auto'}}>
               <EventSideBar />
-              <Profile />
+              <Profile user_id={user_id}/>
               <ChatSideBar />
           </div>
       </Box>
