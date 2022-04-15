43cf079bce0241cde33d1ba6de1a74511182e5e5
mavibirdesmi <b21945795@cs.hacettepe.edu.tr>
2022-04-15 11:05:06 +0300

    added profile link

--- placeholder-frontend/src/pages/HomePageScreen.js
+++ placeholder-frontend/src/pages/HomePageScreen.js
@@ -10,7 +10,7 @@ export default function HomePageScreen( {user} ) {
 
   return (
     <Box sx={{display:'flex', marginLeft:'10px', marginRight:'10px', flexDirection:'column'}}>
-      <TopBar />
+      <TopBar userObj={user}/>
       <div style={{display:'flex', height:'80vh',margin:'0px', justifyContent:'space-between', paddingTop:'2%',
       fontFamily:'Poppins', flex:'auto'}}>
         <EventSideBar />
