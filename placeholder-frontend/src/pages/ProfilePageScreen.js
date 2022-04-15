c248ce479a1cc0a4333ddffe81824444b8a81d9a
mirzadal <mirzadal4646@gmail.com>
2022-04-15 07:33:43 +0300

    c

--- placeholder-frontend/src/pages/ProfilePageScreen.js
+++ placeholder-frontend/src/pages/ProfilePageScreen.js
@@ -5,17 +5,21 @@ import TopBar from '../components/homepage-screen/TopBar';
 import EventSideBar from '../components/homepage-screen/EventSideBar';
 import ChatSideBar from '../components/homepage-screen/ChatSideBar';
 import Profile from "../components/profilepage-screen/Profile"
+import Box from "@mui/material/Box";
 
 const theme = createTheme();
 
 export default function ProfilePageScreen() {
 
   return (
-    <div>
-      <TopBar/>
-      <div>
-        <Profile/>
-      </div>
-    </div>
+      <Box sx={{display:'flex', marginLeft:'10px', marginRight:'10px', flexDirection:'column'}}>
+          <TopBar />
+          <div style={{display:'flex', height:'80vh',margin:'0px', justifyContent:'space-between', paddingTop:'2%',
+              fontFamily:'Poppins', flex:'auto'}}>
+              <EventSideBar />
+              <Profile />
+              <ChatSideBar />
+          </div>
+      </Box>
   )
 }
