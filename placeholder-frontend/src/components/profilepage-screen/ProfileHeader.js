d83203a854d2794d074d406ac56d476cf12d9f86
Tuana Cetinkaya <b21985164@cs.hacettepe.edu.tr>
2022-05-05 23:34:46 +0300

    'idk what i'm doing so here's a checkpoint' commit
    i'm gonna break some stuff real hard in the next 5 minutes so this is just to save the last stable version. may god bless placeholder

--- placeholder-frontend/src/components/profilepage-screen/ProfileHeader.js
+++ placeholder-frontend/src/components/profilepage-screen/ProfileHeader.js
@@ -1,24 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import S3FileUpload from "react-s3";
 import { IconButton, Menu, MenuItem } from "@mui/material";
 import { CameraAlt, MoreHoriz } from "@mui/icons-material";
+
+import { getS3Info } from "../../services/S3Service";
 
 import defaultCover from "../../img/defaultProfileCover.png";
 import defaultProfilePic from "../../img/defaultProfilePic.png";
 
 import "./Profile.css";
+import FileUploader from "../commons/FileUploader";
 
-export default function ProfileHeader({
-  cover,
-  profilePic,
-  profileOwned,
-  sessionUser,
-  user,
-}) {
-  const [anchorEl, setAnchorEl] = React.useState(null);
+export default function ProfileHeader({ profileOwned, sessionUser, user }) {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [profilePic, setProfilePic] = useState(
+    user.profile_pic_path ? user.profile_pic_path : defaultProfilePic
+  );
+  const [coverPic, setCoverPic] = useState(
+    user.cover_url ? user.cover_url : defaultCover
+  );
+  // const [state, setState] = useState({
+  //   mainState: "initial", //initial, uploaded
+  //   selectedFile: null,
+  //   imageUploaded: 0,
+  // });
+
   const open = Boolean(anchorEl);
-  const profilePicPath = user.profile_pic_path
-    ? user.profile_pic_path
-    : defaultProfilePic;
 
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -27,33 +34,46 @@ export default function ProfileHeader({
     setAnchorEl(null);
   };
 
-  const handleChangePP = () => {
-    console.log(JSON.parse(sessionUser.user_type) < 2);
+  const handleChangePP = (selectedFile) => {
+    setProfilePic(selectedFile);
+    // TODO updateUser();
   };
+
+  // const handleChangePP = (file) => {
+  //   // Bucket Details: S3
+  //   const config = {
+  //     bucketName: getS3Info.S3_BUCKET,
+  //     dirName: getS3Info.folder_name, //it’s optional
+  //     region: getS3Info.S3_REGION,
+  //     accessKeyId: getS3Info.S3_KEY_ID,
+  //     secretAccessKey: getS3Info.S3_SECRET_KEY,
+  //   };
+
+  //   // then upload the file
+  //   S3FileUpload.uploadFile(file, config)
+  //     .then((data) => {
+  //       console.log(data.location); // it return the file url
+  //     })
+  //     .catch((err) => {
+  //       alert(err);
+  //     });
+  // };
   const handleChangeCover = () => {
-    console.log("change cover pp");
+    // TODO
   };
 
   return (
     <div
       className="headerContainer"
       style={{
-        backgroundImage: `url(${cover ? cover : defaultCover})`,
+        backgroundImage: `url(${coverPic})`,
       }}
     >
       <div className="ppContainer">
         <div className="profileImage">
-          <img src={profilePicPath} alt="Profile of User" />
+          <img src={profilePic} alt="Profile of User" />
           {profileOwned && (
-            <IconButton
-              className="editPPButton"
-              color="primary"
-              aria-label="upload picture"
-              component="span"
-              onClick={handleChangePP}
-            >
-              <CameraAlt style={{ color: "#A5A5A5" }} />
-            </IconButton>
+            <FileUploader setUploadedFile={setProfilePic}></FileUploader>
           )}
         </div>
       </div>
---
+++ placeholder-frontend/src/services/S3Service.js
@@ -0,0 +1,7 @@
+export const getS3Info = {
+  S3_BUCKET: "",
+  folder_name: "",
+  S3_REGION: "",
+  S3_KEY_ID: "",
+  S3_SECRET_KEY: "",
+};
