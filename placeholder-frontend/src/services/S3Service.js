9f97d10ff07289aff26e0a342cee9d5a45da75c6
Tuana Cetinkaya <b21985164@cs.hacettepe.edu.tr>
2022-05-07 01:54:03 +0300

    'i'll start sth new' commit

--- placeholder-frontend/src/services/S3Service.js
+++ placeholder-frontend/src/services/S3Service.js
@@ -2,30 +2,32 @@
 // import { uploadFile } from "react-s3";
 import S3FileUpload from "react-s3";
 import { updateUser } from "./UserService";
-import { Buffer } from "buffer";
-
+// import { Buffer } from "buffer";
+window.Buffer = window.Buffer || require("buffer").Buffer;
 const config = {
   bucketName: "placeholder-file-bucket",
   region: "eu-central-1",
   accessKeyId: process.env.REACT_APP_ACCESS_KEY_ID,
   secretAccessKey: process.env.REACT_APP_SECRET_ACCESS_KEY,
-  signatureVersion: "v4",
+  //   signatureVersion: "v4",
 };
 
 export function uploadPicture(file, type, setEdited) {
-  S3FileUpload.uploadFile(file, { ...config, dirName: type }, window.Buffer)
+  console.log("uploaded file: ", file);
+  S3FileUpload.uploadFile(file, { ...config, dirName: "/profile-pics" })
     .then((data) => {
       //TODO: change proflePic to type and see if it works
-      updateUser({ profilePic: data.location }).then((response) => {
-        if (response.data.code === 200) {
-          console.log("File uploaded to server!");
-          setEdited(true);
-        } else {
-          alert("We had a problem uploading your file");
-        }
-      });
+      console.log("s3 data:", data.location);
+      //   updateUser({ profilePic: data.location }).then((response) => {
+      //     if (response.data.code === 200) {
+      //       console.log("File uploaded to server!");
+      //       setEdited(true);
+      //     } else {
+      //       alert("We had a problem uploading your file");
+      //     }
+      //   });
     })
     .catch((error) => {
-      alert(error);
+      alert("S3 Had an Error: ", error);
     });
 }
